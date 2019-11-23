9c63675163e7388610c12fc1d616b2fc26b56d70
CarlosCL98 <camer.cl98@gmail.com>
2019-11-22 21:11:47 -0500

    Realizado navbar responsive.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import NavBar from "./components/Navbar";
 
 function App() {
   return (
-    <div>
-      
+    <div className="App">
+      <NavBar />
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,28 @@
 import React from "react";
+import "../sass/Navbar.scss";
 
 class NavBar extends React.Component {
 
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <nav>
-
-            </nav>
-        );
-    }
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">Domicilios Test</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <div className="userHello">
+            <p>Hola! Juan</p>
+          </div>
+        </div>
+      </nav>
+    );
+  }
 }
+
+export default NavBar;
